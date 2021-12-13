53954ecd176e6718c4564ca9b84c95b53f2a1162
nunnah67 <95344880+nunnah67@users.noreply.github.com>
2021-12-13 14:57:08 +0200

    Delete function computeConsumption(){.js

--- src/components/function computeConsumption(){.js
+++
@@ -1,8 +0,0 @@
-function computeConsumption(){
-    let distance = document.getElementById('distance').value;
-    let fuel = document.getElementById('fuel').value;
-    let costper = document.getElementById('costper').value;
-    let consumed =  (fuel / (distance/100)).toFixed(2);
-    let costper100 = (consumed * costper).toFixed(2);
-    document.getElementById('consumption').innerHTML = "Consumption is "+consumed+" Litres/100km ( $ " + costper100 + " / 100km ) <br><br>Total Fuel cost = $ " + (fuel*costper).toFixed(2);
-  }
